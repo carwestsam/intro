fe8194e4878cc9ab459b8f87c3f20df5c48dcd15
Carwest Sung <carwestsam@gmail.com>
2015-07-28 23:26:19 +0800

    isosceles Triangle

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -9,8 +9,43 @@ public class Main {
     public static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        triangle_exercises();
+        //triangle_exercises();
+
+        diamond_exercises();
     }
+
+    private static void diamond_exercises() {
+        isosceles_triangle_wrapper();
+    }
+
+    private static void isosceles_triangle_wrapper() {
+        System.out.println("please input the centered triangle height:");
+        int height = read_positive_int();
+        isosceles_triangle(height);
+    }
+
+    private static void isosceles_triangle(int height) {
+        for ( int i = 0; i<height; i++ ){
+            draw_central_stars(i + 1, height);
+        }
+    }
+
+    private static void draw_central_stars(int stars, int width) {
+        for ( int i=0; i< width - stars; i++ )
+            System.out.print(" ");
+        for ( int i=0; i< 1 + (stars - 1)*2; i++ )
+            System.out.print("*");
+        for ( int i=0; i< width - stars; i++ )
+            System.out.print(" ");
+        System.out.println();
+    }
+
+
+    /****************************************************/
+    /*
+    /*    start of triangle_exercises
+    /*
+    /****************************************************/
 
     private static void triangle_exercises() {
         draw_horizontal_line_wrapper();
@@ -32,8 +67,8 @@ public class Main {
 
     private static void draw_vertical_line_wrapper() {
         System.out.println("please input the length of vertical line:");
-        int n = read_positive_int();
-        draw_vertical_line(n);
+        int number = read_positive_int();
+        draw_vertical_line(number);
     }
 
     private static int read_positive_int() {
@@ -50,8 +85,8 @@ public class Main {
 
     private static void draw_horizontal_line_wrapper() {
         System.out.println("please input the length of horizontal line:");
-        int n= read_positive_int();
-        draw_horizontal_line(n);
+        int number= read_positive_int();
+        draw_horizontal_line(number);
     }
 
     private static void draw_horizontal_line(int number) {
