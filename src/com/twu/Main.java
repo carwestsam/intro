79f65e2fa1c578a3636c151eec25ce17f683dd03
Carwest Sung <carwestsam@gmail.com>
2015-07-28 22:16:15 +0800

    draw a horizontal line

--- src/com/twu/Main.java
+++ src/com/twu/Main.java
@@ -1,8 +1,22 @@
 package com.twu;
 
+
+import java.util.Scanner;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-        System.out.println("*");
+        Scanner in=new Scanner(System.in);
+        System.out.println("please input the number of stars:");
+        int n=in.nextInt();
+        print_stars(n);
+    }
+
+    private static void print_stars(int number) {
+        for ( int i=0; i<number; i++ ){
+            System.out.print("*");
+        }System.out.println("");
     }
 }
